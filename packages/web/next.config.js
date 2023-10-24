11a5b72e48fc573cf9838dec7e13d3057cdcbf85
im-luka <lux.9833@gmail.com>
2023-10-24 11:40:17 +0200

    web/fix: pwa configuration

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,17 +1,52 @@
+const path = require('path');
 const { composePlugins, withNx } = require('@nx/next');
+const withPWAInit = require('next-pwa');
+const isDev = process.env.NODE_ENV !== 'production';
 
-const runtimeCaching = require('next-pwa/cache');
-const withPWA = require('next-pwa')({
-  disable: process.env.NODE_ENV === 'development',
+const withPWA = withPWAInit({
+  disable: isDev,
   dest: 'public',
-  runtimeCaching,
+  exclude: [
+    ({ asset }) => {
+      if (
+        asset.name.startsWith('server/') ||
+        asset.name.match(
+          /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
+        ) ||
+        (isDev && !asset.name.startsWith('static/runtime/'))
+      ) {
+        return true;
+      }
+      return false;
+    },
+  ],
 });
+
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   nx: {
     svgr: false,
+  },
+  webpack(config) {
+    const registerJs = path.join(
+      path.dirname(require.resolve('next-pwa')),
+      'register.js'
+    );
+    const entry = config.entry;
+    config.entry = () =>
+      entry().then((entries) => {
+        if (entries['main-app'] && !entries['main-app'].includes(registerJs)) {
+          if (Array.isArray(entries['main-app'])) {
+            entries['main-app'].unshift(registerJs);
+          } else if (typeof entries['main-app'] === 'string') {
+            entries['main-app'] = [registerJs, entries['main-app']];
+          }
+        }
+        return entries;
+      });
+    return config;
   },
 };
 
