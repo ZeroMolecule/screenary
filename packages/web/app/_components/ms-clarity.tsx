e20739e27b6b5049442bfdc0e9e86d4ea5b158a8
im-luka <lux.9833@gmail.com>
2023-10-25 09:26:07 +0200

    web/feat: clarity id - env

--- packages/web/app/_components/ms-clarity.tsx
+++ packages/web/app/_components/ms-clarity.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react';
 import Script from 'next/script';
+import { ENV } from '@/env';
 
 const code = `
   (function(c,l,a,r,i,t,y){
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-  })(window, document, "clarity", "script", "${process.env.CLARITY_ID}");
+  })(window, document, "clarity", "script", "${ENV.CLARITY_ID}");
 `;
 
 export const MsClarity: FC = () => {
