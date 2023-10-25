1ec83084814e5dafe0cff5a181c659e415c18683
im-luka <lux.9833@gmail.com>
2023-10-25 16:04:09 +0200

    web/fix: next config - dev env

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -3,26 +3,26 @@ const { composePlugins, withNx } = require('@nx/next');
 const withNextIntl = require('next-intl/plugin')();
 const withPWAInit = require('next-pwa');
 const { withSentryConfig } = require('@sentry/nextjs');
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV === 'development';
 
-// const withPWA = withPWAInit({
-//   disable: isDev,
-//   dest: 'public',
-//   exclude: [
-//     ({ asset }) => {
-//       if (
-//         asset.name.startsWith('server/') ||
-//         asset.name.match(
-//           /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
-//         ) ||
-//         (isDev && !asset.name.startsWith('static/runtime/'))
-//       ) {
-//         return true;
-//       }
-//       return false;
-//     },
-//   ],
-// });
+const withPWA = withPWAInit({
+  disable: isDev,
+  dest: 'public',
+  exclude: [
+    ({ asset }) => {
+      if (
+        asset.name.startsWith('server/') ||
+        asset.name.match(
+          /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
+        ) ||
+        (isDev && !asset.name.startsWith('static/runtime/'))
+      ) {
+        return true;
+      }
+      return false;
+    },
+  ],
+});
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -35,25 +35,25 @@ const nextConfig = {
     hideSourceMaps: true,
     widenClientFileUpload: true,
   },
-  // webpack(config) {
-  //   const registerJs = path.join(
-  //     path.dirname(require.resolve('next-pwa')),
-  //     'register.js'
-  //   );
-  //   const entry = config.entry;
-  //   config.entry = () =>
-  //     entry().then((entries) => {
-  //       if (entries['main-app'] && !entries['main-app'].includes(registerJs)) {
-  //         if (Array.isArray(entries['main-app'])) {
-  //           entries['main-app'].unshift(registerJs);
-  //         } else if (typeof entries['main-app'] === 'string') {
-  //           entries['main-app'] = [registerJs, entries['main-app']];
-  //         }
-  //       }
-  //       return entries;
-  //     });
-  //   return config;
-  // },
+  webpack(config) {
+    const registerJs = path.join(
+      path.dirname(require.resolve('next-pwa')),
+      'register.js'
+    );
+    const entry = config.entry;
+    config.entry = () =>
+      entry().then((entries) => {
+        if (entries['main-app'] && !entries['main-app'].includes(registerJs)) {
+          if (Array.isArray(entries['main-app'])) {
+            entries['main-app'].unshift(registerJs);
+          } else if (typeof entries['main-app'] === 'string') {
+            entries['main-app'] = [registerJs, entries['main-app']];
+          }
+        }
+        return entries;
+      });
+    return config;
+  },
 };
 
 const sentryWebpackPluginOptions = {
@@ -65,6 +65,6 @@ const sentryWebpackPluginOptions = {
 const withSentry = (config) =>
   withSentryConfig(config, sentryWebpackPluginOptions);
 
-const plugins = [withNx, withNextIntl, withSentry];
+const plugins = [withNx, withPWA, withNextIntl, withSentry];
 
 module.exports = composePlugins(...plugins)(nextConfig);
