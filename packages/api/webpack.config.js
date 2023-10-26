dc6b79c7e38de86ec64e44bd9748f4005050203c
im-luka <lux.9833@gmail.com>
2023-10-26 19:25:16 +0200

    web/feat: sentry webpack plugin

--- packages/api/webpack.config.js
+++ packages/api/webpack.config.js
@@ -1,10 +1,10 @@
 const { composePlugins, withNx } = require('@nx/webpack');
-const WebpackPlugin = require('@sentry/webpack-plugin');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 module.exports = composePlugins(withNx(), (config) => {
   config.devtool = 'source-map';
   config.plugins.push(
-    new WebpackPlugin({
+    new SentryWebpackPlugin({
       org: process.env.SENTRY_ORG,
       project: process.env.SENTRY_API_PROJECT,
       authToken: process.env.SENTRY_AUTH_TOKEN,
