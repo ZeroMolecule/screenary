fb9986ef8f3602809b8d7b1f6612ca415eef1a8c
im-luka <lux.9833@gmail.com>
2023-10-30 09:49:54 +0100

    web/fix: resolved conflict changes

--- packages/web/app/[locale]/page.tsx
+++ packages/web/app/[locale]/page.tsx
@@ -2,6 +2,8 @@ import { useTranslations } from 'next-intl';
 import { Text, Title } from '@mantine/core';
 
 export default function HomePage() {
+  const t = useTranslations('metadata');
+
   return (
     <div>
       <Title order={2} size="h4">
