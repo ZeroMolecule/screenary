d49e565508c73c280d3e09c6dc733426576ff394
im-luka <lux.9833@gmail.com>
2023-10-31 10:00:56 +0100

    web/feat: facebook login

--- packages/web/domain/auth/index.ts
+++ packages/web/domain/auth/index.ts
@@ -1,5 +1,6 @@
 import type { AuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
 
 export const authOptions: AuthOptions = {
   pages: {
@@ -9,6 +10,10 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
     }),
   ],
   session: {
