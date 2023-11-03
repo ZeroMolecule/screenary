24463f24a8f387538eb95b975508b603ac6d40d5
im-luka <lux.9833@gmail.com>
2023-11-03 15:08:00 +0100

    web/fix: middleware redirect protected routes

--- packages/web/middleware.ts
+++ packages/web/middleware.ts
@@ -1,8 +1,20 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import createIntlMiddleware from 'next-intl/middleware';
 import { DEFAULT_LOCALE, LOCALES } from './utils/constants';
+import { paths } from './navigation/paths';
 
-export default function middleware(request: NextRequest) {
+export default async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const isAuthPage =
+    pathname === paths.login() || pathname === paths.register();
+  const token = request.cookies.get('next-auth.session-token')?.value;
+  if (!token && !isAuthPage) {
+    return NextResponse.redirect(new URL(paths.login(), request.url));
+  }
+  if (token && isAuthPage) {
+    return NextResponse.redirect(new URL(paths.home(), request.url));
+  }
+
   return createIntlMiddleware({
     locales: LOCALES,
     defaultLocale: DEFAULT_LOCALE,
