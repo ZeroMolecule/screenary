90c13aa22bff06f030229161cf177c46de2119c0
im-luka <lux.9833@gmail.com>
2023-11-03 13:21:16 +0100

    web/fix: cmp styling instead of custom css

--- packages/web/app/[locale]/(auth)/login/page.tsx
+++ packages/web/app/[locale]/(auth)/login/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { Stack } from '@mantine/core';
+import { Card, Stack } from '@mantine/core';
 import { SocialButton } from '@/app/_components/auth/social-button';
 import { useTranslations } from 'next-intl';
 import logoWhite from '@/public/images/logo-white.png';
@@ -16,11 +16,13 @@ export default function LoginPage() {
         alt={t('logoAlt')}
         priority
       />
-      <Stack className="login-socials-block">
-        <SocialButton provider="apple" />
-        <SocialButton provider="google" />
-        <SocialButton provider="facebook" />
-      </Stack>
+      <Card p="xl" bg="white" radius="lg">
+        <Stack>
+          <SocialButton provider="apple" />
+          <SocialButton provider="google" />
+          <SocialButton provider="facebook" />
+        </Stack>
+      </Card>
     </Stack>
   );
 }
