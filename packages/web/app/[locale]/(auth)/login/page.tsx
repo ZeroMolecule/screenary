942b3cedd6c4cc8391a9446b4eb0b08876cb4b01
im-luka <lux.9833@gmail.com>
2023-11-03 13:15:40 +0100

    web/fix: logo as .png instead of .svg

--- packages/web/app/[locale]/(auth)/login/page.tsx
+++ packages/web/app/[locale]/(auth)/login/page.tsx
@@ -2,14 +2,20 @@ import Image from 'next/image';
 import { Stack } from '@mantine/core';
 import { SocialButton } from '@/app/_components/auth/social-button';
 import { useTranslations } from 'next-intl';
-import logoWhite from '@/public/images/logo-white.svg';
+import logoWhite from '@/public/images/logo-white.png';
 
 export default function LoginPage() {
   const t = useTranslations('auth');
 
   return (
     <Stack align="center" my="auto">
-      <Image src={logoWhite} alt={t('logoAlt')} priority />
+      <Image
+        src={logoWhite}
+        width={208}
+        height={64}
+        alt={t('logoAlt')}
+        priority
+      />
       <Stack className="login-socials-block">
         <SocialButton provider="apple" />
         <SocialButton provider="google" />
