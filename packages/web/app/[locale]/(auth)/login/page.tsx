bb4ed76f2ccb3a501be514767ccda78aab039797
im-luka <lux.9833@gmail.com>
2023-11-03 13:10:11 +0100

    web/fix: static image import

--- packages/web/app/[locale]/(auth)/login/page.tsx
+++ packages/web/app/[locale]/(auth)/login/page.tsx
@@ -2,19 +2,14 @@ import Image from 'next/image';
 import { Stack } from '@mantine/core';
 import { SocialButton } from '@/app/_components/auth/social-button';
 import { useTranslations } from 'next-intl';
+import logoWhite from '@/public/images/logo-white.svg';
 
 export default function LoginPage() {
   const t = useTranslations('auth');
 
   return (
     <Stack align="center" my="auto">
-      <Image
-        src="/images/logo-white.svg"
-        width={208}
-        height={64}
-        alt={t('logoAlt')}
-        priority
-      />
+      <Image src={logoWhite} alt={t('logoAlt')} priority />
       <Stack className="login-socials-block">
         <SocialButton provider="apple" />
         <SocialButton provider="google" />
