d7a0d9576a93ca260c4c3929ce62e79a5ff0e015
im-luka <lux.9833@gmail.com>
2023-11-09 12:46:07 +0100

    web/feat: login & projects title

--- packages/web/app/[locale]/()/(pages)/projects/page.tsx
+++ packages/web/app/[locale]/()/(pages)/projects/page.tsx
@@ -1,6 +1,21 @@
+import type { Metadata } from 'next';
 import { Box } from '@mantine/core';
+import { getTranslator } from 'next-intl/server';
 import { withPrivatePage } from '@/app/_hoc/with-private-page';
 import { ProjectsWrapper } from '@/app/_components/projects/projects-wrapper';
+
+type Params = { locale: string };
+
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const t = await getTranslator(locale, 'projects');
+  return {
+    title: t('title'),
+  };
+}
 
 function ProjectsPage() {
   return (
