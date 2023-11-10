1be03b5cd077e7eb2414285139d3cb3fa19c533f
im-luka <lux.9833@gmail.com>
2023-11-10 16:09:14 +0100

    web/fix: removed notification popover

--- packages/web/app/_components/notifications-widget/index.tsx
+++ packages/web/app/_components/notifications-widget/index.tsx
@@ -28,7 +28,6 @@ export const NotificationsWidget: FC<Props> = (props) => {
             initialDate={new Date()}
           />
         </Group>
-        <NotificationsPopover />
       </Group>
     </Card>
   );
