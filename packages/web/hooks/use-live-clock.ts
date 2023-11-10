79caa67a2f5a901b3c3f44c6561ff7d8193ca975
im-luka <lux.9833@gmail.com>
2023-11-10 14:29:16 +0100

    web/fix: initial date

--- packages/web/hooks/use-live-clock.ts
+++ packages/web/hooks/use-live-clock.ts
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { DateTimeFormat, formatDate } from '@/utils/datetime';
 
-export const useLiveClock = (initialDate?: Date | string) => {
-  const [date, setDate] = useState(initialDate ?? new Date());
+export const useLiveClock = (initialDate: Date | string = new Date()) => {
+  const [date, setDate] = useState(initialDate);
 
   const formatter = (format: DateTimeFormat) => formatDate(date, format);
 
