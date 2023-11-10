f9282ca8112cd685fedbdd73c252fe936b045ad1
im-luka <lux.9833@gmail.com>
2023-11-10 12:38:29 +0100

    web/fix: date variable

--- packages/web/hooks/use-live-clock.ts
+++ packages/web/hooks/use-live-clock.ts
@@ -2,18 +2,17 @@ import { useEffect, useState } from 'react';
 import { DateTimeFormat, formatDate } from '@/utils/datetime';
 
 export const useLiveClock = () => {
-  const [currentDateTime, setCurrentDateTime] = useState(new Date());
+  const [date, setDate] = useState(new Date());
 
-  const formatter = (format: DateTimeFormat) =>
-    formatDate(currentDateTime, format);
+  const formatter = (format: DateTimeFormat) => formatDate(date, format);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentDateTime(new Date());
+      setDate(new Date());
     }, 1000);
 
     return () => clearInterval(intervalId);
   }, []);
 
-  return { currentDateTime, formatter };
+  return { date, formatter };
 };
