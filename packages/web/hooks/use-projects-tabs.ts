de7319febade8645c78d599db3b73490b0509772
im-luka <lux.9833@gmail.com>
2023-11-17 10:41:34 +0100

    web/fix: project tab params

--- packages/web/hooks/use-projects-tabs.ts
+++ packages/web/hooks/use-projects-tabs.ts
@@ -4,13 +4,24 @@ import { Project } from '@prisma/client';
 import { TabOption } from '@/app/_components/projects-tabs';
 import { projectsQuery } from '@/domain/queries/projects-query';
 import { Data } from '@/domain/remote/response/data';
+import { useSearchParams } from 'next/navigation';
+import { usePathname, useRouter } from '@/navigation';
+
+const PROJECT_TAB_PARAMS_KEY = 'tab';
 
 export const useProjectsTabs = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   const { data: projects } = useQuery<Data<Project[]>>({
     queryKey: projectsQuery.key,
   });
 
-  const [selectedProject, setSelectedProject] = useState(projects?.data[0]);
+  const [selectedProject, setSelectedProject] = useState(() => {
+    const id = searchParams.get(PROJECT_TAB_PARAMS_KEY);
+    return id ? projects?.data.find((p) => p.id === id) : projects?.data[0];
+  });
 
   const tabs: TabOption[] =
     projects?.data.map(({ id, name }) => ({
@@ -19,6 +30,11 @@ export const useProjectsTabs = () => {
     })) ?? [];
 
   const handleChange = (value: string | null) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (value) {
+      params.set(PROJECT_TAB_PARAMS_KEY, value);
+    }
+    replace(`${pathname}?${params.toString()}`);
     setSelectedProject(projects?.data.find((p) => p.id === value));
   };
 
