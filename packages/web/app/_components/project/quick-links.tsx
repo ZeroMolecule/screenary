325aa675f92b3f48f74fc893b621d536d12a7836
im-luka <lux.9833@gmail.com>
2023-12-01 14:34:04 +0100

    web/feat: wip - quick links

--- packages/web/app/_components/project/quick-links.tsx
+++ packages/web/app/_components/project/quick-links.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
 import { useTranslations } from 'next-intl';
-import { Box, Card, Stack } from '@mantine/core';
+import { ActionIcon, Box, Card, Group, Stack } from '@mantine/core';
 import styles from '@/styles/components/quick-links.module.scss';
+import { Text } from '../base/text';
+import { IconPlus } from '@tabler/icons-react';
 
 type Props = {
   projectId: string;
@@ -13,7 +15,20 @@ export const QuickLinks: FC<Props> = (props) => {
   return (
     <Box h="100%">
       <Card h="100%" radius={24} pos="unset" className={styles.quickLinksCard}>
-        <Stack></Stack>
+        <Stack>
+          <Group justify="space-between">
+            <Text size="lg" fw={600}>
+              {t('title')}
+            </Text>
+            <ActionIcon
+              variant="transparent"
+              color="var(--mantine-color-neutral-9)"
+              // onClick={handleCreate}
+            >
+              <IconPlus />
+            </ActionIcon>
+          </Group>
+        </Stack>
       </Card>
     </Box>
   );
