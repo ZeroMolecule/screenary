2a5d24bd1b6cc678dfaf00baf9e90a12d429e5b6
im-luka <lux.9833@gmail.com>
2023-12-04 14:11:52 +0100

    web/fix: notification widget as client component

--- packages/web/app/[locale]/()/(pages)/projects/[id]/page.tsx
+++ packages/web/app/[locale]/()/(pages)/projects/[id]/page.tsx
@@ -1,6 +1,5 @@
-import { getServerSession } from 'next-auth';
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
-import { Project, projectQuery } from '@/domain/queries/project-query';
+import { projectQuery } from '@/domain/queries/project-query';
 import { getQueryClient } from '@/domain/queries/server-query-client';
 import { ProjectPage as ClientProjectPage } from '@/app/_components/project/project-page';
 import { withPrivatePage } from '@/app/_hoc/with-private-page';
@@ -9,32 +8,27 @@ import { tasksQuery } from '@/domain/queries/tasks-query';
 import { TaskStatus } from '@prisma/client';
 import { PageContainer } from '@/app/_components/page-container';
 import { NotificationsWidget } from '@/app/_components/notifications-widget';
-import { authOptions } from '@/domain/auth';
-import { Data } from '@/domain/remote/response/data';
 
 type Params = { locale: string; id: string };
 type Props = { params: Params };
 
 async function ProjectPage({ params: { id } }: Props) {
-  const { username, projectName, dehydratedState } = await useProjectPage(id);
+  const { dehydratedState } = await useProjectPage(id);
 
   return (
     <HydrationBoundary state={dehydratedState}>
       <PageContainer>
         <ClientProjectPage />
       </PageContainer>
-      <NotificationsWidget username={username} projectName={projectName} />
+      <NotificationsWidget projectId={id} />
     </HydrationBoundary>
   );
 }
 
 async function useProjectPage(id: string) {
-  const session = await getServerSession(authOptions);
   const queryClient = getQueryClient();
-  const project = await queryClient.fetchQuery<Data<Project>>({
-    queryKey: projectQuery.key(id),
-  });
   await Promise.all([
+    queryClient.prefetchQuery({ queryKey: projectQuery.key(id) }),
     queryClient.prefetchQuery({ queryKey: notesQuery.key(id) }),
     queryClient.prefetchQuery({
       queryKey: tasksQuery.key(id, {
@@ -49,11 +43,7 @@ async function useProjectPage(id: string) {
   ]);
   const dehydratedState = dehydrate(queryClient);
 
-  return {
-    username: session?.user?.name,
-    projectName: project.data.name,
-    dehydratedState,
-  };
+  return { dehydratedState };
 }
 
 export default withPrivatePage(ProjectPage);
