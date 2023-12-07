0865715a28551ac1408c61b53bb8c23056a5df43
im-luka <lux.9833@gmail.com>
2023-12-07 15:10:50 +0100

    api/fix: quick link & directory create schema

--- packages/api/src/quick-links/dtos/create-quick-link.dto.ts
+++ packages/api/src/quick-links/dtos/create-quick-link.dto.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const createQuickLinkSchema = z.object({
   url: z.string().url(),
-  directoryId: z.string().optional(),
+  directoryId: z.string().nullish(),
 });
 
 export type CreateQuickLinkDto = z.infer<typeof createQuickLinkSchema>;
