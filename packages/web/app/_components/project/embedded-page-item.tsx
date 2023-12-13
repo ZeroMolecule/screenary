dea7da0571ae54b7054a56aa79588f934b5f089e
im-luka <lux.9833@gmail.com>
2023-12-13 14:18:15 +0100

    web/feat: removed embedded page tooltip

--- packages/web/app/_components/project/embedded-page-item.tsx
+++ packages/web/app/_components/project/embedded-page-item.tsx
@@ -1,6 +1,6 @@
 import { Dispatch, FC, SetStateAction } from 'react';
 import Image from 'next/image';
-import { Card, Tooltip } from '@mantine/core';
+import { Card } from '@mantine/core';
 import { IconLink } from '@tabler/icons-react';
 import { EmbeddedPage } from '@prisma/client';
 import styles from '@/styles/components/embedded-pages.module.scss';
@@ -14,29 +14,27 @@ export const EmbeddedPageItem: FC<Props> = (props) => {
   const { title, favicon, handleOnClick } = useEmbeddedPageItem(props);
 
   return (
-    <Tooltip label={title} position="right">
-      <Card
-        w={64}
-        h={64}
-        p={0}
-        radius="lg"
-        pos="relative"
-        className={styles.item}
-        onClick={handleOnClick}
-      >
-        {favicon ? (
-          <Image
-            loader={() => favicon}
-            src={favicon}
-            fill
-            alt={title ?? ''}
-            unoptimized
-          />
-        ) : (
-          <IconLink size={32} color="var(--mantine-color-primary-5)" />
-        )}
-      </Card>
-    </Tooltip>
+    <Card
+      w={64}
+      h={64}
+      p={0}
+      radius="lg"
+      pos="relative"
+      className={styles.item}
+      onClick={handleOnClick}
+    >
+      {favicon ? (
+        <Image
+          loader={() => favicon}
+          src={favicon}
+          fill
+          alt={title ?? ''}
+          unoptimized
+        />
+      ) : (
+        <IconLink size={32} color="var(--mantine-color-primary-5)" />
+      )}
+    </Card>
   );
 };
 
