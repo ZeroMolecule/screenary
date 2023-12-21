18ab95d2d4402443639362ab971feb390d623bbc
im-luka <lux.9833@gmail.com>
2023-12-21 10:58:59 +0100

    api/feat: directories reordering

--- packages/api/src/directories/directories.service.ts
+++ packages/api/src/directories/directories.service.ts
@@ -4,6 +4,7 @@ import { CreateDirectoryDto } from './dtos/create-directory.dto';
 import { UpdateDirectoryDto } from './dtos/update-directory.dto';
 import { PaginationQuery } from '../shared/decorators/pagination-query.decorator';
 import { FindManyDirectoryDto } from './dtos/find-many-directory.dto';
+import { ReorderItemsDto } from '../shared/dtos/reorder-items.dto';
 
 @Injectable()
 export class DirectoriesService {
@@ -35,6 +36,29 @@ export class DirectoriesService {
     });
   }
 
+  async updateMany(dto: ReorderItemsDto, projectId: string, userId: string) {
+    const ids = dto.map((el) => el.id);
+
+    return this.prismaService.$transaction(async (tx) => {
+      const directories = await tx.directory.findMany({
+        where: { id: { in: ids }, projectId, userId },
+      });
+
+      await tx.directory.deleteMany({
+        where: { id: { in: ids }, projectId, userId },
+      });
+
+      const reorderedDirectories = directories.map((directory) => ({
+        ...directory,
+        order:
+          dto.find((el) => el.id === directory.id)?.order ?? directory.order,
+      }));
+      await tx.directory.createMany({ data: reorderedDirectories });
+
+      return reorderedDirectories;
+    });
+  }
+
   async findOne(id: string, projectId: string, userId: string) {
     return this.prismaService.directory.findFirst({
       where: {
@@ -60,6 +84,9 @@ export class DirectoriesService {
       this.prismaService.directory.findMany({
         where,
         ...pagination,
+        orderBy: {
+          order: 'asc',
+        },
       }),
       this.prismaService.directory.count({ where }),
     ]);
