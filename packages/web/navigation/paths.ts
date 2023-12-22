3339f11778cd094c7e90b39be217e45f5a06d630
im-luka <lux.9833@gmail.com>
2023-12-22 09:53:40 +0100

    web/feat: removed inbox page and paths

--- packages/web/navigation/paths.ts
+++ packages/web/navigation/paths.ts
@@ -7,6 +7,5 @@ export const paths = {
   embeddedPage: (projectId: string, pageId: string) =>
     `/projects/${projectId}/embedded-pages/${pageId}`,
   todo: () => '/to-do',
-  inbox: () => '/inbox',
   calendar: () => '/calendar',
 };
