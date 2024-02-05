f01e4a34c5e162999705210146c3df47e5d28ee6
kruno <krunoslav.jurenec@zeromolecule.com>
2024-02-05 16:03:48 +0100

    fix: fix nextjs build v1

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 if [ "$PACKAGE" = "api" ]; then  
     nx run api:build:production
 elif [ "$PACKAGE" = "web" ]; then
+    if [ "$APP_ENV" = "production" ]; then
+        echo "NEXT_PUBLIC_REMOTE_API_BASE_URL=https://screenary-api-e92c7280adc2.herokuapp.com/api" > .env    
+    fi
     nx run web:build:production
     rm -r ./dist/packages/web/.next/cache
 fi
